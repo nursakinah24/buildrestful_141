f78116e1f8ac3eaf5e3291c1e537773978971495
Nur Sakinah <nursakinah2402@gmail.com>
2022-12-02 18:25:55 +0700

    create Product Exception Controller

---
+++ src/main/java/pws/a/learningRESTful/ProductExceptionController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pws.a.learningRESTful;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ *
+ * @author Asus
+ */
+@ControllerAdvice
+public class ProductExceptionController {
+    
+}
