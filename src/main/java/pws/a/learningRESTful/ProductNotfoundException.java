703bc2cece5295da0cf71d941873e8dcc99e9b1d
Nur Sakinah <nursakinah2402@gmail.com>
2022-12-05 21:37:22 +0700

    adding comment on not found exception class

--- src/main/java/pws/a/learningRESTful/ProductNotfoundException.java
+++ src/main/java/pws/a/learningRESTful/ProductNotfoundException.java
@@ -5,7 +5,6 @@
 package pws.a.learningRESTful;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,11 +12,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  * @author Asus
  */
+//Define a class that extends the RuntimeException class
 public class ProductNotfoundException extends RuntimeException {
+    //
     private static final long serialVersionUID = 1L;
-    
+    //annotation that used to define the Request URI to access the REST Endpoints and request method PUT
     @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    //updateProduct method
     public ResponseEntity<Object> updateProduct(){
+        //to throw exception from the API
         throw new ProductNotfoundException();
     }
+    
 }
